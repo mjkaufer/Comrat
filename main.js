f02798c81bd47a3d3f7d0f659061b7c99517cb05
Matthew Kaufer <matthew@kaufer.org>
2014-09-27 19:19:21 -0400

    req.body and github test

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.post('/post', function(req, res){
 	console.log(req.body);
+	res.status(200);
+	res.end("Great");
 });
 
 app.get('/', function(req, res){
@@ -10,6 +18,7 @@ app.get('/', function(req, res){
 	res.end("Hi");
 });
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+
+var server = app.listen(port, function() {
+    console.log('Listening on port ' + port);
 });
