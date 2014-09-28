31c3ac3595f7790825278e4e7bd4307cc321e3a3
Matthew Kaufer <matthew@kaufer.org>
2014-09-27 23:40:47 -0400

    Push and pull working, I hope

--- main.js
+++ main.js
@@ -2,12 +2,67 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
+var fs = require('fs');
+// var config = require('config.json');
+var events = {
+	"push": function(body){
+		var pusher = body.pusher.name;
+		var amount = commits.length;
+		var repo = body.repository.name;
+		if(amount==20)//github cuts off the commits pushed to 20, so at 20, there could be more
+			amount = "20+";
+		var verb = amount==1 ? "commit" : "commits";
+		//todo - implement pro-pic (not returned in API call, could be done w/ JSON call but might lag :( )
+		growl(pusher + " just pushed " + amount + " " + verb + " in the repo " + repo + "!", {title: "Push in " + repo});
+
+	},
+	"pull_request": function(body){
+		if(action=="labeled" || action=="unlabeled" || action=="synchronize")//ones we don't care about, I hope/think
+			return;
+		var requester = body.pull_request.user.login;
+		var avatar = body.user.avatar_url;
+		var repo = body.pull_request.repo.name;
+		var num = body.number;
+		var action = body.action;
+
+		growl(requester + " just " + action + " pull request #" + num + " in " + repo, {title: "Pull in " + repo, image:avatar});
+
+	},
+	"issues": function(body){
+
+	},
+	"member": function(body){
+
+	}
+};
+
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/post', function(req, res){
-	console.log(req.body);
+	var event = req.header('X-Github-Event');//name of the event
+	//we'll handle...
+	/*
+		push (push)
+		pull request (pull_request)
+		issues (issues)
+		member addition (member)
+		//maybe team addition in a bit
+		we'll add a config file too
+
+	*/
+
+	// console.log(req.body);
+	// console.log("----");
+	// console.log(event);
+
+
+	if(events[event] !== undefined)
+		events[event]();//run the event if it exists
+
+
 
 	res.status(200);
 	res.end();
